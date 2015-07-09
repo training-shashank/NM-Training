ea5e693a746076e1798d0803f8045ba7608f5808
training-harshad <shashank@networkmarvels.com>
2015-07-09 15:27:47 +0530

    0907-15:26 minor logging, sorting, printing, few free

--- Harshad/SortMarks.c
+++ Harshad/SortMarks.c
@@ -30,6 +30,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 /******************************************************************************/
 
 #define MAX_SIZE 100
@@ -48,34 +49,52 @@ struct StudListType{
 
 typedef struct StudListType StudListType;
 
+/**************************** Global Declarations *****************************/
 char *err_msg;
+/******************************************************************************/
+
+
+/**************************** Function Declarations ***************************/
+int FileCheck(FILE *);
+int ValidateParam(char *,char *);
+void CreateList(char *, int);
+void SortList(int);
+void PrintList();
+/******************************************************************************/
 
 int main(int argc, char* argv[]){
 
-    FILE *file_ptr;
-    char ch;
-    int i, f_size;
+    FILE *file_ptr, *log_ptr;   // pointers to file & log respectively.
+    char ch;                    // character to be read from file.
+    int i;
+    char buff[20];              // temporary buffer to store date
 
     err_msg = (char *) malloc(MAX_SIZE);
+    log_ptr = fopen("SORT_MARKS_LOG.txt", "a");
 
     if (argc == 2){
         // if the argument is to print help for the program
         if (strcmp(argv[1], "--help") == 0){
             file_ptr = fopen("SORTMARKS_README.txt", "r"); //read from file
-            
-            fseek(file_ptr, 0, SEEK_END);
-
-            f_size = ftell(file_ptr);
-            
-            if ( f_size == 0){
-                printf("File is empty\n");
-                exit(0);
-            }
-            else{
+         
+            /* Check whether file is empty or not */
+            if ( !FileCheck(file_ptr) ){
                 fseek(file_ptr, 0, SEEK_SET);
                 while ((ch = fgetc(file_ptr)) != EOF){
                     printf("%c", ch);
                 }
+            }
+            else{
+                /* Putting in the log */
+                time_t now = time(NULL);
+                strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
+                printf("\n%s\n", err_msg);
+                fputs(buff, log_ptr);
+                fputs("\t", log_ptr);
+                fputs("Error", log_ptr);
+                fputs("\t", log_ptr);
+                fputs(err_msg, log_ptr);
+                exit(0);
             }
             fclose(file_ptr);
         }
@@ -83,113 +102,82 @@ int main(int argc, char* argv[]){
     }
 
     if (argc == 3){
+
+        /* Read input from file */
         if ( strcmp(argv[1], "-f") == 0){
             file_ptr = fopen(argv[2], "r");
             
-            if ( file_ptr == NULL ){
-                strcpy(err_msg, "\nFile couldn't be opened\n");
-                printf("%s", err_msg);
-                exit(0);
-            }
-            else{
+            /*Start reading from file */
+            if ( !FileCheck(file_ptr) ){
+                    
+                fseek(file_ptr, 0, SEEK_SET);
+                printf("Taking input from file\n");
+                    
+                char *name, *marks;
+                int count = 0;
+                    
+                printf("Reading input from file\n");
+                name = (char *) malloc(MAX_SIZE);
 
-                fseek(file_ptr, 0, SEEK_END);
-                f_size = ftell(file_ptr);
-                if ( f_size == 0){
-                    printf("File is empty.\n");
-                    exit(0);
-                }
-                else{
-                    fseek(file_ptr, 0, SEEK_SET);
-                    printf("Taking input from file\n");
-                    char *name;
-                    int marks, count = 0;
-                    name = (char *) malloc(MAX_SIZE);
-                    printf("Reading input from file\n");
-                    while( fscanf(file_ptr, "%s %d", name, &marks) != EOF){
-                        printf("Variables read: %s %d\n", name, marks);
-                        StudListType *node;
-                    
-                        node = (StudListType *) malloc(sizeof (StudListType));
-                        node->name = (char *) malloc(strlen(name));
-                    
-                        strcpy(node->name, name);
-                        node->marks = marks;
-                    
-                        if ( head == NULL ){
-                            head = node;
-                            prev_node = head;
-                        }
-                        else{
-                            prev_node->nxtStudPtr = node;
-                        }
-                        node->nxtStudPtr = NULL;
-                        prev_node = node;
+                /* Create linked list */
+                while( fscanf(file_ptr, "%s %s", name, marks) != EOF){
+                    printf("Variables read: %s %s\n", name, marks);
+                        
+                    if ( ValidateParam(name, marks) ){
+                        CreateList(name, atoi(marks));
                         count++;
                     }
-                    printf("Count: %d\n", count);
-                    /* Sorting linked list */
-                    if ( head != NULL){
-                        printf("Sorting linked list\n");
-                        StudListType *node, *temp;
-   
-                        for( i = 0; i < count; i++){
-    
-                            prev_node = head;
-                            node = prev_node;
-                            temp = node->nxtStudPtr;
-
-                            printf("Prev node: %s %d %p\n",prev_node->name, 
-                                prev_node->marks, (void *) node->nxtStudPtr);
-                            printf("node: %s %d %s %p\n",node->name,
-                                node->marks, (void *) node->nxtStudPtr);
-
-                            while(temp != NULL){
-                                if ( node->marks < temp->marks ){
-                                    //printf("Swapping\n");
-                                    if ( node == head ){
-                                        printf("Swapping start\n");
-                                        node->nxtStudPtr = temp->nxtStudPtr;
-                                        temp->nxtStudPtr = node;
-                                        prev_node = temp;
-                                        temp = node->nxtStudPtr;
-                                        head = prev_node;
-                                    }
-                                    else{
-                                        printf("Swapping inbetween\n");
-                                        prev_node->nxtStudPtr = temp;
-                                        node->nxtStudPtr = temp->nxtStudPtr;
-                                        temp->nxtStudPtr = node;
-                                        prev_node = temp;
-                                        temp = node->nxtStudPtr;
-                                    }
-                                }
-                                else{
-                                    prev_node = node;
-                                    node = temp;
-                                    temp = temp->nxtStudPtr;
-                                }   
-                            }// end of while
-                        }// end of for
-    
-                        /* printing sorted linked list */
-                        printf("Printing linked list\n");
-                        prev_node = head;
-                        while ( prev_node != NULL ){
-                            printf("%s %d",prev_node->name, prev_node->marks);
-                            printf("\n");
-                            prev_node = prev_node->nxtStudPtr;
-                        }
-
-                    }// end of if
-                }// end of else
-            }// end of else
-        }// end of if
-    }// end of if
+                    else{
+                        printf("\n%s\n", err_msg);
+                        //printf("Entering into log\n");
+                        //printf("%s",log_ptr);
+                        
+                        if(log_ptr != NULL){
+                            //printf("Entering into log\n");
+                            time_t now = time(NULL);
+                            printf("debug 2\n");
+                            //strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
+                            //printf("\n%s\n", err_msg);
+                            printf("debug 3\n");
+                            fputs(buff, log_ptr);
+                            fputs("\t", log_ptr);
+                            printf("debug 4\n");
+                            fputs("Error", log_ptr);
+                            printf("debug 5\n");
+                            fputs("\t", log_ptr);
+                            printf("debug 6\n");
+                            fputs(err_msg, log_ptr);
+                            printf("Log updated\n");
+                            }
+                        exit(0);
+                    }
+                }
+                free(name);
                     
+                printf("Count: %d\n", count);
+
+                /* Sorting linked list */
+                SortList(count);
+
+            }// end of if
+            else{
+                printf("\n%s\n", err_msg);
+                time_t now = time(NULL);
+                strftime(buff, 20, "%Y-%m-%d %H:%M:%S", localtime(&now));
+                printf("\n%s\n", err_msg);
+                fputs(buff, log_ptr);
+                fputs("\t", log_ptr);
+                fputs("Error", log_ptr);
+                fputs("\t", log_ptr);
+                fputs(err_msg, log_ptr);
+                exit(0);
+            }
+        }// end of else
+    }// end of if
+    
+    PrintList();    
     free(err_msg);
-    //TakeInput();
-    //sortMarks();
+    fclose(log_ptr);
     return 0;
 }
 /*
@@ -200,102 +188,194 @@ int main(int argc, char* argv[]){
 /******************************************************************************/
 
 /*******************************************************************************
- * Function: TakeInput()
- * working: dynamically allocates the memory according to the number of students
- * to the pointer of structure StudListType & stores their information.
- * size is the number of students in the class.
- *******************************************************************************
- 
+ * Function: FileCheck()
+ * Working: This function checks whether mentioned file exists or not & if
+ * exists whether it's empty or not.
+ * Input: function takes as input file pointer & returns the result whether file
+ * can be used or not.
+ * Return value: 1 or 0.
+ ******************************************************************************/
 
-/******************************************************************************
-void TakeInput(){
-/******************************************************************************
+/******************************************************************************/
+int FileCheck(FILE *file_ptr){
+/******************************************************************************/
 
-    printf("How many students:\n");
-	scanf("%d",&size);
-   
-	printf("Enter the student's information:\n");
-   
-    StudListType *head; 
-    StudListType *stud = (StudListType *)malloc(sizeof(StudListType));
-    head = stud;
+    if ( file_ptr == NULL){
+        strcpy(err_msg, "\nFile doesn't exists\n");
+        return 1;
+    }
+    else{    
+        fseek(file_ptr, 0, SEEK_END);
 
-    for(i=0;i<size;i++){
-        if (i>0){
-            StudListType *ptr = (StudListType *)malloc(sizeof(StudListType));
-            printf("\nStudent %d name: ", i+1);
-    		scanf("%s", ptr->studName);
-            printf("\nStudent %d marks: ", i+1);
-            scanf("%d", ptr->marks);
-            stud->nxtStudPtr = ptr;
-            ptr->nxtStudPtr = NULL;
-            stud = ptr;
+        int f_size = ftell(file_ptr);
+
+        /* print error message */
+        if ( f_size == 0){
+            strcpy(err_msg, "File is empty");
+            return 1;
         }
+        else
+            return 0;
+    }   
+}
+/*******************************************************************************
+ * Function: ValidateParam()
+ * Working: Validates for the parameter read from the file. If parameters are
+ * valid returns 1 else 0;
+ * Input: name & marks read from file.
+ * Return value: 1 or 0.
+ ******************************************************************************/
+
+/******************************************************************************/
+int ValidateParam(char *name, char *marks){
+/******************************************************************************/
+
+    int name_len, i_marks, i = 0;
+    
+    name_len = strlen(name);
+
+    /* checking whether name consists of only alphabets */
+    while ( i < name_len ){
+        if ( isalpha(name[i++]) )
+            continue;
         else{
-             printf("\nStudent %d name: ", i+1);
-             scanf("%s", stud->studName);
-             printf("\nStudent %d marks: ", i+1);
-             scanf("%d", stud->marks);
-             stud->nxtStudPtr = NULL;
-	    }
+            strcpy(err_msg, "Name is not in right format");
+            return 0;
+        }
     }
-    StudListType *ptr = (StudListType *)malloc(sizeof(StudListType));
-    ptr = head;
-    while(ptr != NULL){
-        printf("\nStudent's name: %s", ptr->studName);
-        ptr = ptr->nxtStudPtr;
+
+    /* checking whether marks are only numbers */
+    if ( sscanf(marks, "%i", &i_marks) )
+        return 1;
+    else{
+        strcpy(err_msg, "Marks should be in number format");
+        return 0;
     }
 }
-/*
-/******************************************************************************
- * Function: SortMarks()
- * working: Sorts the linked list studList structure according to the marks of
- * students.
- ******************************************************************************
+
+/*******************************************************************************
+ * Function: CreateList()
+ * Working: Creates the node of type StudListType & adds it to the list
+ * Input: name & marks read from file.
+ ******************************************************************************/
+
+/******************************************************************************/
+void CreateList(char *name, int marks){
+/******************************************************************************/
+
+    StudListType *node;
+                    
+    node = (StudListType *) malloc(sizeof (StudListType));
+    node->name = (char *) malloc(strlen(name));
+    strcpy(node->name, name);
+    node->marks = marks;
+                    
+    if ( head == NULL ){
+        head = node;
+        prev_node = head;
+    }
+    else{
+        prev_node->nxtStudPtr = node;
+    }
+    node->nxtStudPtr = NULL;
+    prev_node = node;
+}
+
+/*******************************************************************************
+ * Function: SortList()
+ * Working: Sorts the linked list of type SstudListType structure according to
+ * the marks.
+ * Input: Count of records in file.
+ ******************************************************************************/
+
+/******************************************************************************/
+void SortList(int count){
+/******************************************************************************/
+
+    int i;
+    if ( head != NULL){
+        printf("Sorting linked list\n");
+        StudListType *node, *temp;
+   
+        for( i = 0; i < count; i++){
+    
+            prev_node = head;
+            node = prev_node;
+            temp = node->nxtStudPtr;
+
+            /*
+            printf("Prev node: %s %d %p\n",prev_node->name, 
+                    prev_node->marks, (void *) node->nxtStudPtr);
+            printf("node: %s %d %s %p\n",node->name,
+                    node->marks, (void *) node->nxtStudPtr);
+            */
+
+            while(temp != NULL){
+                if ( node->marks < temp->marks ){
+                    if ( node == head ){
+                        //printf("Swapping start\n");
+                        node->nxtStudPtr = temp->nxtStudPtr;
+                        temp->nxtStudPtr = node;
+                        prev_node = temp;
+                        temp = node->nxtStudPtr;
+                        head = prev_node;
+                    }
+                    else{
+                        //printf("Swapping inbetween\n");
+                        prev_node->nxtStudPtr = temp;
+                        node->nxtStudPtr = temp->nxtStudPtr;
+                        temp->nxtStudPtr = node;
+                        prev_node = temp;
+                        temp = node->nxtStudPtr;
+                    }
+                }
+                else{
+                    prev_node = node;
+                    node = temp;
+                    temp = temp->nxtStudPtr;
+                }   
+            }// end of while
+        }// end of for
+    }//end of if
+}
+
+/*******************************************************************************
+ * Function: PrintList()
+ * Working: Prints the sorted linked list.
+ ******************************************************************************/
  
+/******************************************************************************/
+void PrintList(){
+/******************************************************************************/
 
-/******************************************************************************
-void SortMarks(){
-/******************************************************************************
+    int limit, i = 1;
+    StudListType *temp;
+    
+    /* printing sorted linked list */
+    printf("Printing linked list\n");
+    
+    printf("\nHow many toppers you want to see:\n");
+    scanf("%d", &limit);
 
-	printf("\nHow many toppers you want:\n");
-    scanf("%d",&limit);
-    printf("Toppers are:\n\n");
+    prev_node = head;
 
-    StudListType *head = studList;
-    StudListType *temp = head;
-	//Sorting marks descendingly
-	
-	for(i = 0; i < size; i++){
-		for(j = i+1; j < size; j++){
-			if(studList[i].marks<studList[j].marks){
-				marks[i] = marks[i] + marks[j];
-				marks[j] = marks[i] - marks[j];
-				marks[i] = marks[i] - marks[j];
-			}
-		}
-		if(i == limit)
-			break;
-		else
-			printf("%d\n",marks[i]);                                //Printing top-n marks
-	}
-	/*
-	printf("\nSorted marks:\n");
-	for(i=0;i<size;i++)
-	printf("%d\n",marks[i]);
-	//marks = temp;*
+    printf("Name\n");
+    printf("----\n");
+    
+    while ( prev_node != NULL && i <= limit ){
+
+        printf("%s",prev_node->name);
+        printf("\n");
+        temp = prev_node;
+        prev_node = prev_node->nxtStudPtr;
+        free(temp->name);
+        free(temp);
+        i++;
+    }
 }
 
-/******************************************************************************
-void printToppers(){
-/******************************************************************************
-
-	sortMarks();
-	printf("\nHow many toppers you want:\n");
-	scanf("%d",&k);
-	printf("Toppers are:\n\n");
-	for(i=0;i<k;i++){
-		printf("%d\n",marks[i]);
-	}
-}
-*/
+/******************************************************************************/
+/**                                                                          **/
+/**                                 EOF                                      **/
+/**                                                                          **/
+/******************************************************************************/
