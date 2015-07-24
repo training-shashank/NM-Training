a95d8453a0cab6d0f41f4808f707b06a0bd2dd72
training-harshad <shashank@networkmarvels.com>
2015-07-24 17:47:43 +0530

    partial update according to standards. add log & file check.

--- Harshad/TraverseDirectory.c
+++ Harshad/TraverseDirectory.c
@@ -27,6 +27,7 @@
  *------------------------------------------------------------------------------
  */
 
+/****************************** HEADER FILES **********************************/
 #include<stdio.h>
 #include<sys/types.h>
 #include<dirent.h>
@@ -34,12 +35,95 @@
 #include<sys/stat.h>
 #include<string.h>
 #include<stdlib.h>
+/******************************************************************************/
 
-/* Function to traverse through directories.
- * Takes input path of the directory to be traversed.
- * Traverses through directories & prints the output
- */
+/******************************** MACROS **************************************/
+#define SUCCESS 0
+#define FAILURE 1
+#define HELP_FILE "/home/harshad/dump/text-files/TRAVERSE_READEME.txt"
+#define LOG_FILE "/home/harshad/dump/text-files/traverse_log.txt"
+/******************************************************************************/
 
+/************************** FUNCTION DECLARATIONS *****************************/
+void traverseDirectory(char *);
+/******************************************************************************/
+
+int main(int argc, char* argv[]){
+	char *path,*file;
+    DIR* dir;
+    struct dirent *ent;
+    struct stat states;
+    FILE* fp;
+    char ch;
+    size_t len = 0;
+    char *temp;
+
+    path = (char *)malloc(sizeof(char)*100);
+    
+    //Checking whether command line argument is passed
+    if(argc == 2){
+        
+        //if the argument is to print help for the program
+        if(strcmp(argv[1], "--help") == 0){
+
+                fp = fopen(HELP_FILE,"r");
+                while((ch = fgetc(fp)) != EOF){
+                    printf("%c", ch);
+                }
+        }
+         // Pass the directory argument to traverse
+         else{
+             strcpy(path, argv[1]);
+         }
+    }
+        
+    // if the path is to be read from another file
+    else if(argc == 3){
+
+        if(strcmp(argv[1],"-f") == 0){
+        
+            fp = fopen(argv[2],"r");
+        
+            if(fp != NULL){
+        
+                printf("\nFile opened\n");
+                getline(&path, &len, fp);
+                //strcat(strcat("\"",path),"\"");
+                printf("\nRetrieved path from file is %s\n", strcat(path,"\0"));
+                
+            }
+            else{
+                printf("\nFile couldn't be opened\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else{
+            // print invalid option & update error log
+            exit(EXIT_FAILURE);
+    }
+    
+    else if ( argc == 1 ){
+        printf("\nEnter the directory path to be traversed:\n");
+        scanf("%s",path);
+        printf("\nPath Entered:\n%s",path);
+                    
+    }
+    traverseDirectory(path);
+    free(path);
+}
+
+/*******************************************************************************
+ **                                                                           **
+ **                             LOCAL FUNCTIONS                               **
+ **                                                                           **
+*******************************************************************************/
+
+/*******************************************************************************
+ * Function: traverseDirectory()
+ * Working: Lists the content (files & sub-directories of directory specified.
+ * Input: Path to traverse
+ * Output: All the sub-directories & files in given path.
+ ******************************************************************************/
 void traverseDirectory(char *name){
 	DIR* dir;
 	struct dirent *ent;
@@ -77,65 +161,12 @@ void traverseDirectory(char *name){
 	}
     else{
         printf("\nfile couldn't be opened\n");
-        return ;
     }
 }
 
-int main(int argc, char* argv[]){
-	char *path,*file;
-    DIR* dir;
-    struct dirent *ent;
-    struct stat states;
-    FILE* fp;
-    char ch;
-    size_t len = 0;
-    char *temp;
+/*******************************************************************************
+ **                                                                           **
+ **                                 EOF                                       **
+ **                                                                           **
+*******************************************************************************/
 
-    path = (char *)malloc(sizeof(char)*100);
-    
-    //Checking whether command line argument is passed
-    if(argc == 2){
-        
-        //if the argument is to print help for the program
-        if(strcmp(argv[1], "--help") == 0){
-
-                fp = fopen("TRAVERSE_README.txt","r");
-                while((ch = fgetc(fp)) != EOF){
-                    printf("%c", ch);
-                }
-        }
-         // Pass the directory argument to traverse
-         else{
-             traverseDirectory(argv[1]);
-         }
-    }
-        
-    // if the path is to be read from another file
-    else if(argc == 3){
-
-        if(strcmp(argv[1],"-f") == 0){
-        
-            fp = fopen(argv[2],"r");
-        
-            if(fp != NULL){
-        
-                printf("\nFile opened\n");
-                getline(&path, &len, fp);
-                //strcat(strcat("\"",path),"\"");
-                printf("\nRetrieved path from file is %s\n", strcat(path,"\0"));
-                traverseDirectory(path);
-            }
-            else{
-                printf("\nFile couldn't be opened\n");
-            }
-        }
-    }
-    
-    else{
-        printf("\nEnter the directory path to be traversed:\n");
-        scanf("%s",path);
-        printf("\nPath Entered:\n%s",path);
-                    
-        traverseDirectory(path);
-    }
-}
